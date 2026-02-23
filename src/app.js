43d80efa19d2553061cab92b3360e59f09194437
mkcodev <mikelrow00@gmail.com>
2026-02-23 19:15:38 +0100

    excuse generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Arrays
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+// randomizar
+function getRandomItem(list) {
+  let randomNumber = Math.random() * list.length;
+  let position = Math.floor(randomNumber);
+  return list[position];
+}
+
+// crear excusa
+function makeExcuse() {
+  let part1 = getRandomItem(who);
+  let part2 = getRandomItem(action);
+  let part3 = getRandomItem(what);
+  let part4 = getRandomItem(when);
+
+  let fullExcuse = part1 + " " + part2 + " " + part3 + " " + part4;
+  return fullExcuse;
+}
+
+// insertar
+window.onload = function () {
+  let excuseElement = document.querySelector("#excuse");
+  let excuseText = makeExcuse();
+  excuseElement.innerHTML = excuseText;
 };
